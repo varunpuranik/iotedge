ffa777291bf79b1d1759f9d3da06cc18689672c0
Arnav Singh <arsing@microsoft.com>
2018-09-11 16:01:06 -0700

    Allow overriding env vars for RunCommandValidator

--- edge-agent/test/Microsoft.Azure.Devices.Edge.Agent.Docker.E2E.Test/RunCommandValidator.cs
+++ edge-agent/test/Microsoft.Azure.Devices.Edge.Agent.Docker.E2E.Test/RunCommandValidator.cs
@@ -1,5 +1,6 @@
 ﻿namespace Microsoft.Azure.Devices.Edge.Agent.Docker.E2E.Test
 {
+    using System.Collections.Generic;
     using System.Diagnostics;
 
     public class RunCommandValidator : Validator
@@ -7,6 +8,8 @@
         public string Command { get; set; }
 
         public string Args { get; set; }
+
+        public Dictionary<string, string> Env { get; set; }
 
         public string OutputEquals { get; set; }
 
@@ -26,6 +29,14 @@
                     RedirectStandardOutput = true
                 }
             };
+
+            if (this.Env != null)
+            {
+                foreach (var kvp in this.Env)
+                {
+                    process.StartInfo.Environment[kvp.Key] = kvp.Value;
+                }
+            }
 
             string output = "";
             process.OutputDataReceived += (sender, args) => output += args.Data;
