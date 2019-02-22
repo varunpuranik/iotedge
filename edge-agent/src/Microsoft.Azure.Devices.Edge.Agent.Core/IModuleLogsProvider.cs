f3611e1e5c6cf3f53986ddde14dc85bed86911b8
Varun Puranik <vpuranik@microsoft.com>
2019-02-21 17:42:59 -0800

    Cleanup ModuleLogsProvider

--- edge-agent/src/Microsoft.Azure.Devices.Edge.Agent.Core/IModuleLogsProvider.cs
+++ edge-agent/src/Microsoft.Azure.Devices.Edge.Agent.Core/IModuleLogsProvider.cs
@@ -1,13 +1,14 @@
 // Copyright (c) Microsoft. All rights reserved.
 namespace Microsoft.Azure.Devices.Edge.Agent.Core
 {
-    using System.IO;
-    using System.Threading;
+    using System.Collections.Generic;
     using System.Threading.Tasks;
     using Microsoft.Azure.Devices.Edge.Util;
 
     public interface IModuleLogsProvider
     {
         Task<string> GetLogsAsText(string module, Option<int> tail);
+
+        Task<IEnumerable<ModuleLogsProvider.ModuleLogMessage>> GetLogs(string module, Option<int> tail);
     }
 }
