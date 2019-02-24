215d72b0a805e0db8d6b8c30ab0fd249a1940034
Varun Puranik <vpuranik@microsoft.com>
2019-02-23 19:02:24 -0800

    Blob Logs uploader

--- edge-agent/src/Microsoft.Azure.Devices.Edge.Agent.Core/logs/ILogsUploader.cs
+++ edge-agent/src/Microsoft.Azure.Devices.Edge.Agent.Core/logs/ILogsUploader.cs
@@ -1,10 +1,11 @@
 // Copyright (c) Microsoft. All rights reserved.
 namespace Microsoft.Azure.Devices.Edge.Agent.Core.Logs
 {
+    using System.IO;
     using System.Threading.Tasks;
 
-    public interface IBlobUploader
+    public interface ILogsUploader
     {
-        Task UploadToBlob(string containerUri, string blobName, byte[] payload);
+        Task Upload(string uri, string module, Stream payloadStream);
     }
 }
